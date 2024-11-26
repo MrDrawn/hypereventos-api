0e9683bcaf4b429226f1daee4f006a5a8dc18fae
MrDrawn <lipelimasilva123@gmail.com>
2024-11-26 00:52:44 -0300

    feat: added feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { EventModule } from './event/event.module';
 import { CheckoutModule } from './checkout/checkout.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [PrismaModule, EventModule, CheckoutModule],
+  imports: [PrismaModule, EventModule, CheckoutModule, NotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
